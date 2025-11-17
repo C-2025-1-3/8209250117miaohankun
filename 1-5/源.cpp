aadc3f5108e36d32b4df61215ee3de24db295e88
xinyenozen <3649321763@qq.com>
2025-11-17 15:31:23 +0800

    Add files via upload

---
+++ 1-5/源.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	double c, f;
+	cout << "请输入华氏温度：" << endl;
+	cin >> f;
+	c = (f - 32) * 5 / 9;
+	cout << fixed << setprecision(2) << "摄氏温度为：" << c << endl;
+	return 0;
+}
